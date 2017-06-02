5621aad919e2e918661c4106d2f5b60fa4d5bed3
David Bowers <dbowers.github@gmail.com>
2017-06-01 22:26:13 -0600

    auto focus the input box to send messages and send when the enter key is pressed

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,12 +1,23 @@
 $(() => {
     console.log('document loaded')
 
-    $('#btnSend').click(() => {
+    function sendMessage(){
         let txtMessenger = $('#txtMessenger')
         let message = txtMessenger.val()
 
         App.chat.perform('message', {content: message})
 
         txtMessenger.val('')
+        txtMessenger.focus()
+    }
+
+    $('#btnSend').click(() => {
+       sendMessage()
     })
+
+    $(document).keypress((e) => {
+        if(e.which == 13) {
+            sendMessage()
+        }
+    });
 })
